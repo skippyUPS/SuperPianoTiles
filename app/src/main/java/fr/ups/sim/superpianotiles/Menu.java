a93406a4304c59bf13dddaa69a755a6bcdc57bdc
Zarfoux <vaccaro.quentin@gmail.com>
2016-03-21 22:43:34 +0100

    ajout demenu

---
+++ app/src/main/java/fr/ups/sim/superpianotiles/Menu.java
@@ -0,0 +1,16 @@
+package fr.ups.sim.superpianotiles;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Quentin on 21/03/2016.
+ */
+public class Menu extends View {
+
+
+    public Menu(Context context) {
+        super(context);
+    }
+
+}
