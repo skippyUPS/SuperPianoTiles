be250f6b3abdc7b49e51724a87ec37e6a84a30de
Zarfoux <vaccaro.quentin@gmail.com>
2016-03-22 20:55:10 +0100

    ajout demenu

--- app/src/main/java/fr/ups/sim/superpianotiles/Menu.java
+++ app/src/main/java/fr/ups/sim/superpianotiles/Menu.java
@@ -1,16 +1,35 @@
 package fr.ups.sim.superpianotiles;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Quentin on 21/03/2016.
  */
-public class Menu extends View {
+public class Menu extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
 
-    public Menu(Context context) {
-        super(context);
+        final Button loginButton = (Button) findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, TilesStartActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 
 }
--- app/src/main/java/fr/ups/sim/superpianotiles/TilesStartActivity.java
+++ app/src/main/java/fr/ups/sim/superpianotiles/TilesStartActivity.java
@@ -22,7 +22,7 @@ public class TilesStartActivity extends Activity {
     private final String PROGRESS_BAR_INCREMENT="ProgreesBarIncrementId";
     TilesView tilesView;
     MediaPlayer mPlayer;
-
+    Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
